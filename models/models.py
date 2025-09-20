b39420a6afec1d444566f9593e58ab1bb1cef5fd
= <=>
2025-09-20 16:24:38 -0300

    Add medicine error

--- models/models.py
+++ models/models.py
@@ -1,42 +1,61 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String, TIMESTAMP
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 class Artist(Base):
     __tablename__ = 'artist'
     artist_id = Column('artist_id',Integer, primary_key=True, index = True)
     name = Column('name',String)
 
 class Album(Base):
-    __tablename__ = "Album"
-    AlbumId = Column(Integer, primary_key=True, index = True)
-    Title = Column(String)
-    ArtistId = Column(Integer)
+    __tablename__ = "album"
+    AlbumId = Column('album_id',Integer, primary_key=True, index = True)
+    Title = Column('title',String)
+    ArtistId = Column('artist_id',Integer)
 
 class Track(Base):
-    __tablename__ = "Track"
-    TrackId = Column(Integer, primary_key=True, index=True)
-    Name = Column(String)
-    AlbumId = Column(Integer)
-    MediaTypeId = Column(Integer)
-    GenreId = Column(Integer)
-    Composer = Column(String)
-    Milliseconds = Column(Integer)
-    Byte = Column(Integer)
-    UnitPrice = Column(Integer)
+    __tablename__ = "track"
+    TrackId = Column('track_id',Integer, primary_key=True, index=True)
+    Name = Column('name',String)
+    AlbumId = Column('album_id',Integer)
+    MediaTypeId = Column('media_type_id', Integer)
+    GenreId = Column('genre_id', Integer)
+    Composer = Column('composer_id', String)
+    Milliseconds = Column('milliseconds', Integer)
+    Byte = Column('byte', Integer)
+    UnitPrice = Column('unit_price', Integer)
 
 class Customer(Base):
-    __tablename__ = 'Customer'
-    CustomerId = Column(Integer, primary_key=True, index=True)
-    FirstName = Column(String)
-    LastName = Column(String)
-    Company = Column(String)
-    Address = Column(String)
-    City = Column(String)
-    State = Column(String)
-    Country = Column(String)
-    PostalCode = Column(String)
-    Phone = Column(String)
-    Fax = Column(String)
-    Email = Column(String)
-    SupportRepId = Column(Integer)
+    __tablename__ = 'customer'
+    customer_id = Column('customer_id',Integer, primary_key=True, index=True)
+    first_name = Column('first_name',String)
+    last_name = Column('last_name', String)
+    company = Column('company', String)
+    address = Column('address', String)
+    city = Column('city', String)
+    state = Column('state', String)
+    country = Column('country',String)
+    postal_code = Column('postal_code',String)
+    phone = Column('phone', String)
+    fax = Column('fax',String)
+    email = Column('email', String)
+    support_rep_id = Column('support_rep_id', Integer)
+
+class Employee(Base):
+    __tablename__ = 'employee'
+    employee_id = Column('employee_id', Integer, primary_key=True, index=True)
+    last_name = Column('last_name', String)
+    first_name = Column('first_name', String)
+    title = Column('title', String)
+    reports_to = Column('reports_to', String)
+    birth_date = Column('birth_date', TIMESTAMP)
+    hire_date = Column('hire_date', TIMESTAMP)
+    address = Column('address', String)
+    city = Column('city', String)
+    state = Column('state', String)
+    country = Column('country', String)
+    postal_code = Column('postal_code', String)
+    phone = Column('phone', String)
+    fax = Column('fax', String)
+    email = Column('email', String)
--- routers/product.py
+++ routers/product.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
-from models.schema import ArtistSchema
-from models.models import Artist as ArtistModel
+from models.schema import ArtistSchema, CustomerSchema
+from models.models import Artist as ArtistModel, Customer as CustomerModel
 from data.db import sessionLocal
 
 
@@ -17,5 +18,24 @@ def get_db():
 
 @router.get("/artists", summary="Get all artists", response_model=list[ArtistSchema])
 async def get_artist(db: Session = Depends(get_db)):
-    db_artists = db.scalars(select(ArtistModel)).all()
-    return [ArtistSchema.model_validate(artist) for artist in db_artists]
+    try:
+        db_artists = db.scalars(select(ArtistModel)).all()
+
+        if not db_artists:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='No se encuentran artistas en la base de datos'
+            )
+        return [ArtistSchema.model_validate(artist) for artist in db_artists]
+    except SQLAlchemyError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f'Error: {e}'
+        )
+    
+@router.get('/customer', 
+           summary="Get all customer", 
+           response_model=list[CustomerSchema])
+async def get_customer(db: Session = Depends(get_db)):
+    db_customer = db.scalars(select(CustomerModel).where(CustomerModel.country == 'Brazil')).all()
+    return [CustomerSchema.model_validate(customer) for customer in db_customer]
